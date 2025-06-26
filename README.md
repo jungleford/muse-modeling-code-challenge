21014891ee1cc7c58eb999b18791fb4a58fd2ea7
jungleford <jungleford@gmail.com>
2025-06-26 18:32:45 +0800

    Add a basic sample code from https://docs.jointjs.com/learn/features/ports/ , and remove useless code from the template.

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # muse-modeling-code-challenge
+
+## Requirements
+
+1. You can resize the port at will.
+2. You can drag the port along the border of the rectangle element at will, but the center point of the port cannot leave the border.
+
+Hint: You can use JointJS library.
+
+## Demo
+
+For npm:
+```bash
+npm install
+
+npm run dev
+```
+
+For pnpm:
+```bash
+pnpm install
+
+pnpm run dev
+```
+
+Then open your browser and access http://localhost:5173/
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react';
+import { dia, shapes } from '@joint/core';
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const namespace = shapes;
+    const graph = new dia.Graph({}, { cellNamespace: namespace });
+
+    const paper = new dia.Paper({
+      el: document.getElementById('paper'),
+      width: 1280,
+      height: 800,
+      gridSize: 1,
+      model: graph,
+      background: { color: '#AAAAAA' },
+      cellViewNamespace: namespace,
+      linkPinning: false, // Prevent link being dropped in blank paper area
+      defaultLink: () => new shapes.standard.Link(),
+      defaultConnectionPoint: { name: 'boundary' },
+      validateConnection: function (cellViewS, magnetS, cellViewT, magnetT, end, linkView) {
+        // Prevent linking between ports within one element
+        if (cellViewS === cellViewT) return false;
+      }
+    });
+
+    const port = {
+      label: {
+        position: {
+          name: 'left'
+        },
+        markup: [{
+          tagName: 'text',
+          selector: 'label'
+        }]
+      },
+      attrs: {
+        portBody: {
+          magnet: true,
+          width: 16,
+          height: 16,
+          x: -8,
+          y: -8,
+          fill: '#03071E'
+        },
+        label: {
+          text: 'port'
+        }
+      },
+      markup: [{
+        tagName: 'rect',
+        selector: 'portBody'
+      }]
+    };
+
+    const model = new shapes.standard.Rectangle({
+      position: { x: 275, y: 50 },
+      size: { width: 640, height: 640 },
+      attrs: {
+        body: {
+          fill: '#8ECAE6'
+        }
+      },
+      ports: {
+        items: [port] // add a port in constructor
+      }
+    });
+
+    model.addPort(port); // add a port using Port API
+    graph.addCell(model);
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div id="paper">demo</div>
+  );
 }
 
-export default App
+export default App;
